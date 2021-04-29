a5eb30ad88134c6ebe7e86b156cc1ad3c7abb1bf
Hela <hela.chatti.landolsi@gmail.com>
2021-04-29 02:21:16 +0200

    add previous buttons and its functionnalities

--- scripts/game.js
+++ scripts/game.js
@@ -15,16 +15,30 @@ async function getStory() {
   Si vous êtes chauds : Injecter le contenu du premier chapitre et les choix disponibles dans l'interface
  */
 const containerElement =document.querySelector(".container");
+const previousButton = document.getElementById("previousChapter");
 
 //search for the choosed chapter
 function foundChapter(chapters,target){
     let selectedChapter = chapters.find(chapter => chapter.id == target);
-    selectedChapter ? displayStoryChapter(selectedChapter ) : showEndOfTheStory();
+    if(selectedChapter){
+        displayStoryChapter(selectedChapter ); 
+        saveChapters(selectedChapter) 
+    }else{
+         showEndOfTheStory();
+         saveChapters({}); 
+
+     } ;
     }
-        
+  
+
  //create container for the chapter
- function createStoryContainer(chapterTitle,chapterContent,nextChapters){
-   
+ function createStoryContainer(idChapter,chapterTitle,chapterContent,nextChapters){
+     
+    if(idChapter != 1){
+        previousButton.classList.replace("hidden","visible") ;
+    }
+    else{previousButton.classList.replace("visible","hidden") ; 
+    }
      containerElement.innerHTML = '';
      const title = document.createElement("h1");
      title.innerHTML= chapterTitle;
@@ -38,7 +52,7 @@ function foundChapter(chapters,target){
      const button = document.createElement("button");
      button.textContent = content;
      button.id =`${target}`;
-     button.classList.add("btn","btn-light","mx-2","border","border-secondary");
+     button.classList.add("btn","mx-2");
      button.addEventListener("click" ,displayUserchoice);
      choiceConteneur.append(button);
      }
@@ -47,8 +61,16 @@ function foundChapter(chapters,target){
  }
  //display chapter on the screen
  function displayStoryChapter(data){
-     const{title,content,choices} = data;
-     createStoryContainer(title,content,choices);
+     //saveChapters(data);
+     const{id,title,content,choices} = data;
+     createStoryContainer(id,title,content,choices);
+
+ }
+ let previousChaptersId=[];
+ function saveChapters(data){
+    
+   previousChaptersId.push(data);
+   console.log(previousChaptersId);
  }
 
  
@@ -57,10 +79,11 @@ window.onload= function (){
     getStory().then(datas =>foundChapter(datas,1));
          
      }
- 
- 
  //when we click on a button to choose a chapter
  function displayUserchoice(event){
+     //actualId = id;
+      //previousChaptersId.push(actualId);
+      //console.log(previousChaptersId);
      const userChoice = event.target.id;
      getStory().then(datas =>foundChapter(datas,userChoice));
       
@@ -85,7 +108,37 @@ window.onload= function (){
   //end of the story 
   function showEndOfTheStory(){
     containerElement.innerHTML = `<h1> Le livre est fini </h1>`;
-  }
- 
+    //previousButton.classList.replace("visible","hidden") ;
 
+  }
+  //let actualId=1;
+  //const previousChaptersId=[1];
+  //configurate the previous Button
+ /* function configPreviousButton(idTarget,count){
+    if(idTarget != 1){
+        previousButton.classList.replace("hidden","visible") ;
+        previousChaptersId.push(actualId);
+    }
+    else{
+        previousButton.classList.replace("visible","hidden") 
+    }
+   
+    actualId = idTarget;
+    //console.log(`Button ${previousChaptersId} and prevId ${actualId}`);
+    console.log(previousChaptersId);
+    previousButton.addEventListener("click",function()
+                        { getStory().then(datas =>foundChapter(datas,previousChaptersId.pop()))});
+
+  }*/
+  previousButton.addEventListener("click",function()
+                        { 
+                            console.log(previousChaptersId);
+                            previousChaptersId.pop();
+                            console.log(previousChaptersId);
+                            displayStoryChapter(previousChaptersId[previousChaptersId.length-1]);
+                            
+                            
+                        }
+                            );
+  
 
